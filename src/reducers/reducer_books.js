55f0613adada6d8a7f77e09fb520479508d28f2a
Danilo Miranda <danilokassio@gmail.com>
2017-02-15 23:53:02 -0300

    Implemented containers and components managed by redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default  function() {
+  return [
+    {title: 'JavascriptL The good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 33},
+    {title: 'The Dark Tower', pages: 90},
+    {title: 'Eloquent Ruby', pages: 34}
+  ]
+}
